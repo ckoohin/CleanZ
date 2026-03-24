257d2b5c9f355ba9239bc4e3982cf004cd379c90
ckoohin <chuhien.nhat@gmail.com>
2026-03-24 10:17:28 +0700

    update

--- be/src/common/enums/order-status.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum OrderStatus {
-  PENDING = 'PENDING', // Chờ xác nhận
-  CONFIRMED = 'CONFIRMED', // Đã xác nhận, đang chuẩn bị
-  COMPLETED = 'COMPLETED', // Đã phục vụ
-  CANCELLED = 'CANCELLED', // Đã hủy
-}
